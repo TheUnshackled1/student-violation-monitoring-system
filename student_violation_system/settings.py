b78a73a22126df36f12afcc2225b70552f66226d
John Tyrone Pagunsan Coronel <182468403+TheUnshackled1@users.noreply.github.com>
2025-12-15 20:38:20 +0800

    modified files

--- student_violation_system/settings.py
+++ student_violation_system/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    # "jazzmin",  # Jazzmin MUST be before django.contrib.admin
+    "jazzmin",  # Jazzmin MUST be before django.contrib.admin
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     # "channels",  # WebSocket support for realtime features (temporarily commented)
     "violations",  # Add the violations app
+    
 ]
 
 # =============================================================================
